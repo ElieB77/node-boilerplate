ed0f7d47b96a60303373ebd2cb9ca1e88b177085
Elie Bissor <eliebiss.26@gmail.com>
2024-04-01 14:49:47 +0200

    feat: Add utils for hashing and jwt.

---
+++ src/utils/authentication.ts
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+export const comparePassword = (password: string, hash: string) => {
+  return bcrypt.compare(password, hash);
+};
+
+export const hashPassword = (password: string) => {
+  return bcrypt.hash(password, 5);
+};
+
+export const createJWT = (user) => {
+  const token = jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.JWT_SECRET
+  );
+
+  return token;
+};
