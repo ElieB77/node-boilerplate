f250797a60d02759dffbe9358e7e412cfd8a1522
Elie Bissor <eliebiss.26@gmail.com>
2024-04-11 22:20:29 +0200

    feat: Add asyncHandler

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,27 +1,45 @@
-import prisma from "../db";
 import { Request, Response } from "express";
+import logger from "../logger";
+import prisma from "../database/db";
 import {
   comparePassword,
   createJWT,
   hashPassword,
 } from "../utils/authentication";
+import asyncHandler from "express-async-handler";
 
 export const AuthController = {
-  signup: async (req: Request, res: Response) => {
+  signup: asyncHandler(async (req: Request, res: Response) => {
     const { email, password } = req.body;
 
-    const user = await prisma.user.create({
+    const alreadyExists = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (alreadyExists) {
+      logger.error(
+        `method: ${req.method}, route: ${req.url}, message: User already exists -> ${alreadyExists.email}`
+      );
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const newUser = await prisma.user.create({
       data: {
         email,
         password: await hashPassword(password),
       },
     });
 
-    const token = createJWT(user);
+    const token = createJWT(newUser);
+    logger.info(
+      `method: ${req.method}, route: ${req.url}, message: Signup succesful -> ${newUser.email}`
+    );
     res.status(200).json({ token });
-  },
+  }),
 
-  signin: async (req: Request, res: Response) => {
+  signin: asyncHandler(async (req: Request, res: Response) => {
     const { email, password } = req.body;
 
     const user = await prisma.user.findUnique({
@@ -30,15 +48,26 @@ export const AuthController = {
       },
     });
 
+    if (!user) {
+      logger.error(
+        `method: ${req.method}, route: ${req.url}, message: User not found -> ${user.email}`
+      );
+      return res.status(401).json({ message: "Invalid email or password." });
+    }
+
     const isValid = await comparePassword(password, user.password);
 
     if (!isValid) {
-      res.status(401);
-      res.json({ message: "Invalid email or password." });
-      return;
+      logger.error(
+        `method: ${req.method}, route: ${req.url}, message: Invalid password -> ${user.email}`
+      );
+      return res.status(401).json({ message: "Invalid email or password." });
     }
 
     const token = createJWT(user);
+    logger.info(
+      `method: ${req.method}, route: ${req.url}, message: Logged in succesful -> ${user.email}`
+    );
     res.status(200).json({ token });
-  },
+  }),
 };
