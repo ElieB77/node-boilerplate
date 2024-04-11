939ceb1f164fa6d116068b46feb22a10861267a7
Elie Bissor <eliebiss.26@gmail.com>
2024-04-11 22:22:09 +0200

    feat: Add validate middleware

--- src/routes/authentication.ts
+++ src/routes/authentication.ts
@@ -1,9 +1,21 @@
 import express from "express";
 import { AuthController } from "../controllers/authentication";
+import { checkSchema } from "express-validator";
+import { authenticationSchema } from "../validators/schemas/authenticationSchema";
+import { validate } from "../validators/middlewares/authentication";
 
 const router = express.Router();
 
-router.post("/auth/signup", AuthController.signup);
-router.post("/auth/signin", AuthController.signin);
+router.post(
+  "/signup",
+  validate(checkSchema(authenticationSchema)),
+  AuthController.signup
+);
+
+router.post(
+  "/signin",
+  validate(checkSchema(authenticationSchema)),
+  AuthController.signin
+);
 
 export default router;
