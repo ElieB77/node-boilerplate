0af8c611f7e929c800302439c7c78ad8500c9597
Elie Bissor <eliebiss.26@gmail.com>
2024-04-11 22:15:18 +0200

    feat: Add global errors + startServer function

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,31 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes";
+import * as dotenv from "dotenv";
+import { exceptionHandler } from "./middlewares/globalError";
+import logger from "./logger";
 
 const app = express();
+dotenv.config();
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
 app.use("/", router);
+app.use(exceptionHandler);
 
-// app.use("/api", protect, router);
-// app.post("/user", createNewUser);
-// app.post("/signin", signin);
+const startServer = async () => {
+  app
+    .listen(port, () =>
+      logger.info(`🚀 Server ready at: http://localhost:${port}`)
+    )
+    .on("error", (error) => {
+      logger.error(error.message);
+      process.exit(1);
+    });
+};
+
+startServer();
 
 export default app;
