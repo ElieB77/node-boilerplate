0519324d5013503af83bfbf9f618fc9bd51a0006
Elie Bissor <eliebiss.26@gmail.com>
2024-04-11 22:01:30 +0200

    feat: Add user routes.

---
+++ src/routes/user.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { checkSchema } from "express-validator";
+import { validate } from "../validators/middlewares/authentication";
+import { UserController } from "../controllers/user";
+import { userSchema } from "../validators/schemas/user";
+
+const router = express.Router();
+
+router.get("/users", UserController.getUsers);
+router.get("/:id", validate(checkSchema(userSchema)), UserController.findUser);
+router.delete(
+  "/:id",
+  validate(checkSchema(userSchema)),
+  UserController.deleteUser
+);
+
+export default router;
