317e4b332863f18a3097b8ac75f1f13c7514531d
Elie Bissor <eliebiss.26@gmail.com>
2024-04-11 22:23:41 +0200

    feat: Add protect + authorize middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import authentication from "./authentication";
+import user from "./user";
+import { authorize, protect } from "../middlewares/auth";
 
 const router = express.Router();
 
-router.use("/", authentication);
+router.use("/auth", authentication);
+router.use("/user", protect, authorize("ADMIN"), user);
 
 export default router;
