18782f615621d41cb4dab25b45c23d07d878384b
Elie Bissor <eliebiss.26@gmail.com>
2024-04-11 22:29:10 +0200

    Refactor: Move some files & directories

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import app from "./server";
-import * as dotenv from "dotenv";
-dotenv.config();
-
-const port: number = 3000;
-
-app.listen(port, () => {
-  console.log(`🚀 Server ready at: http://localhost:${port}`);
-});
