a6ed0c0320004346f40cc2e81ad1e70c0750c9bb
Elie Bissor <eliebiss.26@gmail.com>
2024-04-12 11:25:52 +0200

    feat: Add errorHandler util function

---
+++ src/utils/errorHandler.ts
@@ -0,0 +1,15 @@
+interface ResponseError extends Error {
+  statusCode?: number;
+  data?: string | string[];
+}
+
+export const errorHandler: (
+  statusCode: number,
+  message: string,
+  data?: string | string[]
+) => ResponseError = (statusCode, message, data) => {
+  const error: ResponseError = new Error(message);
+  error.statusCode = statusCode;
+  error.data = data;
+  return error;
+};
