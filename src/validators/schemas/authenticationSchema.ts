e91e65564ebc4098c81232407b3ebb23f364f8a7
Elie Bissor <eliebiss.26@gmail.com>
2024-04-12 10:26:05 +0200

    feat: Add more password requirements(1 lowercase, 1 uppercase and 1number)

--- src/validators/schemas/authenticationSchema.ts
+++ src/validators/schemas/authenticationSchema.ts
@@ -2,13 +2,25 @@ import { Schema } from "express-validator";
 
 export const authenticationSchema: Schema = {
   email: {
-    isEmail: true,
-    errorMessage: "Incorrect email format.",
+    notEmpty: {
+      errorMessage: "Email is required",
+    },
+    isEmail: {
+      errorMessage: "Incorrect email format",
+    },
   },
   password: {
+    notEmpty: {
+      errorMessage: "Password is required",
+    },
     isLength: {
-      options: { min: 6 },
-      errorMessage: "Password must have at least 6 characters.",
+      options: { min: 12 },
+      errorMessage: "Password must have at least 12 characters",
+    },
+    matches: {
+      options: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/, "g"],
+      errorMessage:
+        "Password must contain at least one lowercase letter, one uppercase letter, and one number",
     },
   },
 };
