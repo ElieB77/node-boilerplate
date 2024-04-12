1a7e7da75bb9e7a74de68c66daecfea90ee76f15
Elie Bissor <eliebiss.26@gmail.com>
2024-04-12 11:45:25 +0200

    refactor: Remove try / catch block by using asyncHandler instead

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,62 +2,43 @@ import prisma from "../database/db";
 import logger from "../logger";
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
+import asyncHandler from "express-async-handler";
+import { errorHandler } from "../utils/errorHandler";
 
-export const protect = (req: Request, res: Response, next: NextFunction) => {
-  const bearer = req.headers.authorization;
+export const protect = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const bearer = req.headers.authorization;
 
-  if (!bearer) {
-    logger.error(
-      `method: ${req.method}, route: ${req.url}, message: Not authorized`
-    );
-    return res.status(401).json({ message: "Not authorized" });
-  }
+    if (!bearer) {
+      logger.error(
+        `method: ${req.method}, route: ${req.url}, message: Not authorized`
+      );
+      return res.status(401).json({ message: "Not authorized" });
+    }
 
-  const [, token] = bearer.split(" ");
+    const [, token] = bearer.split(" ");
 
-  if (!token) {
-    logger.error(
-      `method: ${req.method}, route: ${req.url}, message: Unvalid token`
-    );
-    return res.status(401).json({ message: "Unvalid token" });
-  }
+    if (!token) {
+      return next(errorHandler(401, "Unvalid token"));
+    }
 
-  try {
     const user = jwt.verify(token, process.env.JWT_SECRET);
     req.user = user;
     next();
-  } catch (error) {
-    logger.error(
-      `method: ${req.method}, route: ${req.url}, message: Unvalid token`
-    );
-    return res.status(401).json({ message: "Unvalid token" });
   }
-};
+);
 
-export const authorize =
-  (requiredRole: string) =>
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const user = await prisma.user.findUnique({
-        where: {
-          id: req.user.id,
-        },
-      });
+export const authorize = (requiredRole: string) =>
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: req.user.id,
+      },
+    });
 
-      if (user.role !== requiredRole) {
-        logger.error(
-          `method: ${req.method}, route: ${req.url}, message: Forbidden. admin only`
-        );
-        return res.status(403).json({ error: "Forbidden. Admin only" });
-      }
-
-      next();
-    } catch (error) {
-      logger.error(
-        `method: ${req.method}, route: ${req.url}, message: ${error}`
-      );
-      res
-        .status(500)
-        .json({ error: "An error occurred while authorizing the user" });
+    if (user.role !== requiredRole) {
+      return next(errorHandler(403, "Forbidden. Admin only"));
     }
-  };
+
+    next();
+  });
