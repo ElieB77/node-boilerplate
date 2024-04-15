04dd8201ab7d74fab1b532fb6ea4531b04708959
Elie Bissor <eliebiss.26@gmail.com>
2024-04-15 11:57:36 +0200

    feat: Add unit test for the utils hashPassword function

--- src/__tests__/unit/utils/authentication.test.ts
+++ src/__tests__/unit/utils/authentication.test.ts
@@ -1,10 +1,10 @@
-import { describe, expect, test } from "@jest/globals";
 import bcrypt from "bcrypt";
-import { comparePassword } from "../../../utils/authentication";
+import { describe, expect, test } from "@jest/globals";
+import { comparePassword, hashPassword } from "../../../utils/authentication";
 
 describe("utility functions for authentication", () => {
   describe("password comparison", () => {
-    test("should return true when password matches hash", async () => {
+    test("should return true when password match hash", async () => {
       const password = "MyPassword";
       const hashedPassword = await bcrypt.hash(password, 5);
       const result = await comparePassword(password, hashedPassword);
@@ -16,6 +16,14 @@ describe("utility functions for authentication", () => {
       const hashedPassword = await bcrypt.hash(password, 5);
       const result = await comparePassword("WrongPassword", hashedPassword);
       expect(result).toBe(false);
+    });
+  });
+
+  describe("password hashing", () => {
+    test("should return the hashed password", async () => {
+      const password = "MyPassword";
+      const hashedPassword = await hashPassword(password);
+      expect(hashedPassword).not.toBe(password);
     });
   });
 });
