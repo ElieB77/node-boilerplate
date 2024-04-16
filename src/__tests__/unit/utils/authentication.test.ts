1439351de1dd477e2ff0da9f3717dbb924450773
Elie Bissor <eliebiss.26@gmail.com>
2024-04-16 10:09:29 +0200

    feat: Add unit test for the utils createJWT function

--- src/__tests__/unit/utils/authentication.test.ts
+++ src/__tests__/unit/utils/authentication.test.ts
@@ -1,9 +1,14 @@
 import bcrypt from "bcrypt";
 import { describe, expect, test } from "@jest/globals";
-import { comparePassword, hashPassword } from "../../../utils/authentication";
+import {
+  comparePassword,
+  createJWT,
+  hashPassword,
+} from "../../../utils/authentication";
+import jwt from "jsonwebtoken";
 
-describe("utility functions for authentication", () => {
-  describe("password comparison", () => {
+describe("Utility functions for authentication", () => {
+  describe("Password Comparison", () => {
     test("should return true when password match hash", async () => {
       const password = "MyPassword";
       const hashedPassword = await bcrypt.hash(password, 5);
@@ -19,11 +24,22 @@ describe("utility functions for authentication", () => {
     });
   });
 
-  describe("password hashing", () => {
+  describe("Password Hashing", () => {
     test("should return the hashed password", async () => {
       const password = "MyPassword";
       const hashedPassword = await hashPassword(password);
       expect(hashedPassword).not.toBe(password);
+    });
+  });
+
+  describe("Generate JWT", () => {
+    test("should include correct user id in the payload", () => {
+      const userId = "1";
+      const token = createJWT(userId);
+      const decoded = jwt.verify(token, process.env.JWT_SECRET) as {
+        id: string;
+      };
+      expect(decoded.id).toBe(userId);
     });
   });
 });
